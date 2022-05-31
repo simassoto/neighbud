855a42d3da30cda3d22fcd95ddbc43b1818a3991
Carlos Soto <soto.simas@gmail.com>
2022-05-31 12:16:46 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :services do
+    resources :bookings, only: [:new, :create, :edit, :update]
+  end
+  devise_for :users do
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
