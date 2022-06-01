d3be1caed5cad0861e3173fb70dc28fa81f162ed
sayonara silva <sayow@hotmail.com>
2022-06-01 15:25:01 +0100

    formulário new bookings ok

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,15 @@
+
+
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+
+export default class extends Controller {
+
+
+  connect() {
+    flatpickr(".datepicker", {
+      minDate: "today"
+  });
+  }
+}
