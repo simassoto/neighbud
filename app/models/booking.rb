d2d8fea7fb38a4fb77656f6086b3f97ac18cd66b
sayonara silva <sayow@hotmail.com>
2022-06-01 14:38:15 +0100

    bookings validates 2

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
-  validates :date, presence: true,
-  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :date, presence: true
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
 end
