4e6d64519dd53699003746ce7e1e9d752b3d81c8
sayonara silva <sayow@hotmail.com>
2022-06-01 15:32:47 +0100

    ja volto

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,12 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :booking
+
+  validates :title
+  validates :description
+  validates :category
+  validates :address
+  validates :price
+
+
 end
