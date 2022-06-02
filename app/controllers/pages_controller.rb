0ebb144b5778919fb1d11635a8bae89ed2a73916
Carlos Soto <soto.simas@gmail.com>
2022-06-02 18:40:49 +0100

    bookings do dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,17 @@
 class PagesController < ApplicationController
 
+
+
   def home
 
   end
 
   def dashboard
     @user = current_user
+    @booked = Booking.where(services: { user: @user }).joins(:service)
   end
+
+
 
   def profile
   end
