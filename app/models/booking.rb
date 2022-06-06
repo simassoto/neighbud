a9adde2531ebeaab7deb6997cbb5a2e64964b39c
sayonara silva <sayow@hotmail.com>
2022-06-06 12:13:22 +0100

    tentativas de validação

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,11 +6,13 @@ class Booking < ApplicationRecord
   validates :date, presence: true
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
-private
+  private
 
-def wallet_money
-    if user.wallet < service.price
+  def wallet_money
+    total_price = @booking.service[:price] * @booking[:duration]
+    if user.wallet < total_price
       errors.add(:base, "Sorry! You don't have enough $NB money to book this service.")
-    end
+     end
   end
+
 end
