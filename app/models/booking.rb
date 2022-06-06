ad78ad8b35e68917dca45390c4453b44b19617e0
Carlos Soto <soto.simas@gmail.com>
2022-06-06 16:57:19 +0100

    link to aprove

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
+  enum status: [:requested, :approved, :rejected, :confirmed]
+
   validates :date, presence: true
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_141833) do
+ActiveRecord::Schema.define(version: 2022_06_06_134534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_06_03_141833) do
     t.bigint "service_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
     t.index ["service_id"], name: "index_bookings_on_service_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
